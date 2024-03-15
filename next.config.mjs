d71989e67fd8519381a86492d10676d106e30f73
Jaret Garcia <jgarciag2005@alumno.ipn.mx>
2024-03-14 22:33:43 -0600

    actions config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com', 'i.ibb.co'],
     },
+    basePath: '/harvestr-nextjs',
+    output: 'export',
 };
 
 export default nextConfig;
