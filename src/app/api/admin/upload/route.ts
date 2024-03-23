8fbb6d79a8658ffc20ff5db746c9c555c5068b61
P0T4TO1 <99705684+P0T4TO1@users.noreply.github.com>
2024-03-22 18:53:31 -0600

    Update route.ts

--- src/app/api/admin/upload/route.ts
+++ src/app/api/admin/upload/route.ts
@@ -10,44 +10,52 @@ cloudinary.config({
 });
 
 export async function POST(request: NextRequest, response: NextResponse) {
-  const data = await request.formData();
-  const file: File | null = data.get("file") as unknown as File;
-
-  if (!file) {
-    return NextResponse.json({ success: false });
-  }
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-
-  // With the file data in the buffer, you can do whatever you want with it.
-  // For this, we'll just write it to the filesystem in a new location
-  //   const filePath = `/tmp/${file.name}`
+  try{
+    const data = await request.formData();
+    const file: File | null = data.get("file") as unknown as File;
   
-  // works in development
-  const filePath = path.join(
-    process.cwd(),
-    "public/images/products",
-    file.name
-  );
-  await writeFile(filePath, buffer);
-  console.log(`open ${filePath} to see the uploaded file`);
-
-  // Upload to Cloudinary
-  const { secure_url } = await cloudinary.uploader.upload(filePath, {
-    folder: "products",
-  });
-
-  // Delete the local file
-  // const unlinkPromise = unlink(filePath);
-  // if (!unlinkPromise) {
-  //   console.log("Error deleting file");
-  // }
-
-  // const localFilePublicPath = `/images/products/${file.name}`;
-
-  return NextResponse.json(
-    { filePath, secure_url },
-    { status: 200 }
-  );
+    if (!file) {
+      return NextResponse.json({ success: false });
+    }
+  
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+  
+    // With the file data in the buffer, you can do whatever you want with it.
+    // For this, we'll just write it to the filesystem in a new location
+    //   const filePath = `/tmp/${file.name}`
+    
+    // works in development
+    const filePath = path.join(
+      process.cwd(),
+      "public/images/products",
+      file.name
+    );
+    await writeFile(filePath, buffer);
+    console.log(`open ${filePath} to see the uploaded file`);
+  
+    // Upload to Cloudinary
+    const { secure_url } = await cloudinary.uploader.upload(filePath, {
+      folder: "products",
+    });
+  
+    // Delete the local file
+    // const unlinkPromise = unlink(filePath);
+    // if (!unlinkPromise) {
+    //   console.log("Error deleting file");
+    // }
+  
+    // const localFilePublicPath = `/images/products/${file.name}`;
+  
+    return NextResponse.json(
+      { filePath, secure_url },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Subir imagen a cloudinary" },
+      { status: 500 }
+    );
+  }
 }
