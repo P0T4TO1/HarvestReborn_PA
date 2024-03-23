97e4cf289b57015c4d33455908561d9402eed701
Jaret Garcia <jgarciag2005@alumno.ipn.mx>
2024-03-23 12:27:22 -0600

    dynamic get products

--- src/app/api/inventory/products/route.ts
+++ src/app/api/inventory/products/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 async function getAllProducts(req: NextRequest, res: NextResponse) {
   const products = await prisma.m_producto.findMany();
