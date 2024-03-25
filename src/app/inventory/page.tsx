327a4a802b27e5f6c5f516fb3148630d8646600c
Jaret Garcia <jgarciag2005@alumno.ipn.mx>
2024-03-25 03:26:38 -0600

    without middleware v0.4

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -6,7 +6,6 @@ import { ProductsInventory } from "@/components";
 const InventoryPage = async () => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/auth/login");
-  if (session.user.id_rol !== 4) redirect("/auth/register?oauth=true");
   return (
     <>
       <section className="flex flex-col relative overflow-hidden min-h-screen">
