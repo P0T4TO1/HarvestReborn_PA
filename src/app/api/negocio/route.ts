dcd32c5263b662db6694811f4cafc70941533a58
P0T4TO1 <99705684+P0T4TO1@users.noreply.github.com>
2024-03-25 14:30:48 -0600

    Make dynamic negocios route

--- src/app/api/negocio/route.ts
+++ src/app/api/negocio/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 async function getAllNegocios(req: NextRequest, res: NextResponse) {
   const negocios = await prisma.m_negocio.findMany({
     include: {
