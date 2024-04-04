d3fde9a705163d06fa64c684f8fbf3adfacd1c6b
Jaret Garcia <jgarciag2005@alumno.ipn.mx>
2024-04-04 14:03:39 -0600

    dark mode fix 2

--- src/components/ui/navbar/DarkModeSwitch.tsx
+++ src/components/ui/navbar/DarkModeSwitch.tsx
@@ -1,30 +1,31 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme as useNextTheme } from "next-themes";
 import { Button } from "@nextui-org/react";
 
-export const DarkModeSwitch = async () => {
+export const DarkModeSwitch = () => {
   const { setTheme, resolvedTheme } = useNextTheme();
+  const [hydrated, setHydrated] = useState(false);
   useEffect(() => {
-    if (resolvedTheme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [resolvedTheme]);
+    setHydrated(true);
+  }, []);
 
   return (
-    <Button
-      isIconOnly
-      variant="light"
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-    >
-      {resolvedTheme === "dark" ? (
-        <span className="material-symbols-outlined">light_mode</span>
-      ) : (
-        <span className="material-symbols-outlined">dark_mode</span>
+    <>
+      {hydrated && (
+        <Button
+          isIconOnly
+          variant="light"
+          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+        >
+          {resolvedTheme === "dark" ? (
+            <span className="material-symbols-outlined">light_mode</span>
+          ) : (
+            <span className="material-symbols-outlined">dark_mode</span>
+          )}
+        </Button>
       )}
-    </Button>
+    </>
   );
 };
