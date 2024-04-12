fe7258ed71d39275050237a1705d88b839851643
P0T4TO1 <jgarciag2005@alumno.ipn.mx>
2024-04-12 03:15:02 -0600

    Required error add product admin

--- src/validations/admin.validation.ts
+++ src/validations/admin.validation.ts
@@ -39,7 +39,9 @@ export const adminAddProductValidation = z.object({
   enTemporada: z.boolean({
     required_error: "El campo en temporada es obligatorio",
   }),
-  categoria: z.enum(["FRUTA", "VERDURA"]),
+  categoria: z.enum(["FRUTA", "VERDURA"], {
+    required_error: "La categoría del producto es obligatoria",
+  }),
 });
 
 export const updateProductValidation = adminAddProductValidation
